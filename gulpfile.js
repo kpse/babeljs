f12d787a1e81438a801f5ef920eb5ca4cbf69748
Suo Qin <kpsesuo@gmail.com>
2016-02-01 21:36:56 +0800

    ignore compiled.js, add gulp clean task

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,14 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
+const del = require('del');
 
-gulp.task('default', () => {
+gulp.task('clean', () => {
+  // You can use multiple globbing patterns as you would with `gulp.src`
+  return del(['dist']);
+});
+
+gulp.task('default', ['clean'], () => {
   return gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel({
