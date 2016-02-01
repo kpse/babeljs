10711201aea218221ee3e0fdda3b2a526be1c1bf
Suo Qin <kpsesuo@gmail.com>
2016-02-01 20:28:36 +0800

    array & object destructure

---
+++ src/app-compiled.js
@@ -0,0 +1,19 @@
+let recevie = (complete = () => console.log('finished!')) => complete();
+
+recevie();
+
+let [first,,,, fifth] = [1, 2, 3, 4, 5];
+
+console.log(first, fifth);
+
+let people = [{ name: 'suoqin', email: 'suoqin@gmail.com' }, { name: 'zhangjie', email: 'zhangjie@gmail.com' }];
+
+let [, zj] = people;
+
+function logEmail({ email }) {
+  console.log(email);
+}
+
+logEmail(zj);
+
+//# sourceMappingURL=app-compiled.js.map
--- src/app.js
+++ src/app.js
@@ -1,3 +1,23 @@
-let recevie = (complete = () => console.log('finished!')) => complete()
+let recevie = (complete = () => console.log('finished!')) => complete();
 
-recevie()
+recevie();
+
+let [first,,,,fifth] = [1,2,3,4,5]
+
+console.log(first, fifth);
+
+let people = [
+  {name: 'suoqin', email: 'suoqin@gmail.com'},
+  {name: 'zhangjie', email: 'zhangjie@gmail.com'}
+
+];
+
+let [,zj] = people;
+
+function logEmail({email}) {
+  console.log(email);
+}
+
+logEmail(zj);
+
+
