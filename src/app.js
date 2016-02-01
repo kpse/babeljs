b5bc0fe5d014a5ffa305c964326e84ed7f07d034
Suo Qin <kpsesuo@gmail.com>
2016-02-01 20:44:39 +0800

    string template

--- src/app.js
+++ src/app.js
@@ -21,3 +21,17 @@ function logEmail({email}) {
 logEmail(zj);
 
 
+let hello = 'hello';
+let world = `${hello} world`;
+
+console.log(world);
+
+function tag(strings, ...values) {
+  let status = (values[0] < 21) ? "awake" : 'sleepy';
+
+  return `${strings[0]}${values[0]}${strings[1]}${status}`
+}
+
+var message = tag`It's ${new Date().getHours()} I'm ${""}`;
+
+console.log(message);
